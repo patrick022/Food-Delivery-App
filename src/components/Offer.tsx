d91f7cc7ec33b545f9d48415d79a89c5635e24fa
Patrick de Guzman <pdeguzman769@gmail.com>
2023-10-01 00:52:06 +0800

    UI only push

---
+++ src/components/Offer.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import CountDown from './CountDown';
+
+const Offer = () => {
+	return (
+		<div className="bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offerBg.png')] md:h-[70vh]">
+			{/* TEXT CONTAINER */}
+			<div className="flex-1 flex flex-col justify-center items-center text-center gap-8 p-6">
+				<h1 className="text-white text-5xl font-bold xl:text-6xl">
+					Delicious Burger & French Fry
+				</h1>
+				<p className="text-white xl:text-xl">
+					Indulge in our mouthwatering limited-time offer! Sink your teeth into
+					our savory, juicy burgers paired perfectly with crispy, golden fries.
+					It is a match made in heaven for your taste buds. Do not miss out on
+					this delectable deal that is here for a limited time only. Satisfy
+					your cravings today!
+				</p>
+				<CountDown />
+				<button className="bg-red-500 text-white rounded-md py-3 px-6">
+					Order Now
+				</button>
+			</div>
+			{/* IMAGE CONTAINER */}
+			<div className="flex-1 w-full relative md:h-full">
+				<Image src="/offerProduct.png" alt="" fill className="object-contain" />
+			</div>
+		</div>
+	);
+};
+export default Offer;
